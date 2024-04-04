cb6ce68f2bb7ab3ca058299b38bd13cff2ad3369
sisco404 <francisco.foresi98@outlook.com>
2024-04-04 17:32:44 +0200

    dataset downloading

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import os
+import opendatasets as od
 
 DIMENSIONS = [10000, 100000, 1000000, 10000000]
-
+DATA_DIR = './data/'
+DATASET_URL = 'https://www.kaggle.com/datasets/ehallmar/daily-historical-stock-prices-1970-2018/download?datasetVersionNumber=1'
+DATASET_NAME = 'daily-historical-stock-prices-1970-2018'
 
 
 def create_files_with_distinct_dimension(path_to_filename: str):
@@ -9,7 +13,7 @@ def create_files_with_distinct_dimension(path_to_filename: str):
     sorted_df = df.sort_values(by=['date'], ascending=True)
     for dim in DIMENSIONS:
         filename = "historical_stock_prices_" + str(dim)
-        path = "insert/path/" + filename + ".csv"
+        path = DATA_DIR + DATASET_NAME + '/' + filename + ".csv"
         new_df = sorted_df.head(dim)
         new_df.to_csv(path_or_buf=path, columns=df.columns, index=False)
 
@@ -22,7 +26,14 @@ def remove_commas_from_sector_names(path_to_filename: str):
     df = df[["ticker", "exchange", "name", "sector", "industry"]]
     df.to_csv(path_to_filename, index=False)
 
+def download_dataset():
+    os.mkdir(DATA_DIR)
+    od.download(dataset_id_or_url=DATASET_URL, data_dir=DATA_DIR)
+    
+
 
 if __name__ == '__main__':
-    remove_commas_from_sector_names("historical_stocks")
-    create_files_with_distinct_dimension("historical_stock_prices.csv")
+    if not os.path.exists(DATA_DIR):
+        download_dataset(DATA_DIR)
+    remove_commas_from_sector_names(DATA_DIR + DATASET_NAME + "/historical_stocks.csv")
+    create_files_with_distinct_dimension(DATA_DIR + DATASET_NAME + "/historical_stock_prices.csv")
